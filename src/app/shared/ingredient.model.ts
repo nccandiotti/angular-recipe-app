d692b1832d9bf9cb47520eaaf37c3f60cf391621
Nicole Candiotti <nccandiotti@gmail.com>
2022-05-12 13:36:30 -0400

    creating and outputting shopping list

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+export class Ingredient {
+  constructor(public name: string, public amount: number) {}
+}
+
+// typescript shortcut for creating a new class, can put accessors, types, and properties all as constructor arguments and forego definining them in class and assigning in constructor body
