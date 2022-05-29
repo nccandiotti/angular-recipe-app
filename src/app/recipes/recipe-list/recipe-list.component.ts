8d18db25edd1ee3d04111a4b09fb21ca07f594c8
Nicole Candiotti <nccandiotti@gmail.com>
2022-05-29 14:56:56 -0400

    event binding for displaying recipes onClick working

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import {Recipe} from "../recipe.model"
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  // not totally sure about this here, using Recipe model we created 
-recipes: Recipe[] = [
-  new Recipe('A Test Recipe', 'This is simply a test', 'https://media-cldnry.s-nbcnews.com/image/upload/t_focal-758x379,f_auto,q_auto:best/rockcms/2022-03/plant-based-food-mc-220323-02-273c7b.jpg')
-]
-  constructor() { }
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  // not totally sure about this here, using Recipe model we created
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://media-cldnry.s-nbcnews.com/image/upload/t_focal-758x379,f_auto,q_auto:best/rockcms/2022-03/plant-based-food-mc-220323-02-273c7b.jpg'
+    ),
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
-
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from './recipe.model';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
 })
 export class RecipesComponent implements OnInit {
-  recipes: []
+  selectedRecipe: Recipe;
+  recipes: [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
